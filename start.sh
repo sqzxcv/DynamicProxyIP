5d9eb4497a1268d94093cf5a80a4657b9a3f6155
ShengQiang <qiang_sheng@qq.com>
2017-08-02 16:20:43 +0800

    修改 start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ source /usr/local/bin/virtualenvwrapper.sh
 
 git pull 
 workon scrapyd_py3.6.1 
-#pip install -r requirements.txt
+pip install -r requirements.txt
 echo "当前执行路径" + $(pwd)
-#pm2 reload start.py -x --interpreter /root/Envs/scrapyd_py3.6.1/bin/python
+pm2 reload start.py -x --interpreter /root/Envs/scrapyd_py3.6.1/bin/python
