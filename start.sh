1f3877e19494fa0ac7dcf717f414483b3e5257cb
ShengQiang <qiang_sheng@qq.com>
2017-08-02 16:06:57 +0800

    x

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd /var/www/DynamicProxyIP/
 git pull 
 workon scrapyd_py3.6.1 
 #pip install -r requirements.txt
-echo "当前执行路径" + pwd
+echo "当前执行路径" + $pwd
 #pm2 reload start.py -x --interpreter /root/Envs/scrapyd_py3.6.1/bin/python
