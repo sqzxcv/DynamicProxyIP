f82da2372672e481af4b0187f6001425c0ae76ef
ShengQiang <qiang_sheng@qq.com>
2017-08-02 16:03:57 +0800

    x

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ git pull
 workon scrapyd_py3.6.1 
 #pip install -r requirements.txt
 echo "当前执行路径" + pwd
-pm2 reload start.py -x --interpreter /root/Envs/scrapyd_py3.6.1/bin/python
+#pm2 reload start.py -x --interpreter /root/Envs/scrapyd_py3.6.1/bin/python
